7d8fb7a4e31ccf3f0d9248848a7026bbe7aead02
Ajsal-fayis <149041863+Ajsal-fayis@users.noreply.github.com>
2026-08-25 15:48:57 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -14,5 +14,22 @@ const projects = [
     description: "Manual testing project covering product catalog, cart management, checkout, payment validation, and order confirmation.",
     highlights: ["60 Test Cases", "Checkout Testing", "Payment Validation", "Test Summary"],
     github: "https://github.com/Ajsal-fayis/QA-Demo-Ecommerce-Manual-Testing"
-  }
+  },
+  {
+    number: "03",
+    title: "Selenium Automation Testing",
+    type: "AUTOMATION TESTING",
+    description: "Test automation project built using Selenium WebDriver and Java to automate and validate web application functionality.",
+    highlights: ["Selenium WebDriver", "Java", "Test Automation", "Web Testing"],
+    github: "https://github.com/Ajsal-fayis/SeleniumAutomationProject"
+},
+{
+    number: "04",
+    title: "API Automation Testing",
+    type: "API AUTOMATION",
+    description: "API automation testing project built using Java, Maven, Rest Assured, and TestNG to validate REST API responses.",
+    highlights: ["Rest Assured", "Java", "TestNG", "Maven"],
+    github: "https://github.com/Ajsal-fayis/API-Automation"
+}
+  
 ];
